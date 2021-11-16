8157e6540d2a643ce6cb5de07b5a7ca5008d27fa
Your Name <you@example.com>
2021-11-16 16:00:08 +0530

    n

---
+++ toasty/src/main/java/papaya/in/toasty/activity/BaseActivity.java
@@ -0,0 +1,30 @@
+package papaya.in.toasty.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.ViewDataBinding;
+
+public abstract class BaseActivity<T> extends AppCompatActivity {
+
+    protected T binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = getActivityBinding();
+        if (binding instanceof ViewDataBinding) {
+            setContentView(((ViewDataBinding) binding).getRoot());
+            init();
+            initCtrl();
+        }
+    }
+
+
+    protected abstract T getActivityBinding();
+
+    protected abstract void init();
+
+    protected abstract void initCtrl();
+
+}
